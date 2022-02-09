86559fbc5fe885d053d8c17afb3ede08a274230c
tuyennv <v.tuyennv18@vinhome.vn>
2022-02-09 08:56:10 +0700

    Xong! Tạo Eureka Client

--- src/main/java/com/tuyennguyen/menudong/service/MenuDongServiceApplication.java
+++ src/main/java/com/tuyennguyen/menudong/service/MenuDongServiceApplication.java
@@ -2,8 +2,10 @@ package com.tuyennguyen.menudong.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MenuDongServiceApplication {
 
     public static void main(String[] args) {
